a3f70d49d4610dd10c12b31ce5900da756d6d0a8
Danijela <Danijela@DESKTOP-OCPCB1S>
2020-08-13 00:27:23 +0200

    Definisanje sql upita - Komunikacija sa bazom podataka(klasa broker)

--- sirupi___za___mrsavljenje/sirupi/bin/sirupi/broker.clj
+++ sirupi___za___mrsavljenje/sirupi/bin/sirupi/broker.clj
@@ -6,3 +6,30 @@
    :subname "//localhost/sirup_za_mrsavljenje"
    :user "root"
    :password ""})
+
+(defn data []
+   (into [] (sql/query connection ["SELECT * FROM narudzbina, sirup WHERE narudzbina.sirupID = sirup.sirupID"])))
+
+
+(defn deleteNar [id]
+  (sql/delete! connection :narudzbina ["narudzbinaid = ?" id])
+  )
+
+(defn dataNarudzbine [id]
+  (into [] (sql/query connection ["SELECT * FROM narudzbina, sirup WHERE narudzbina.sirupID = sirup.sirupID && narudzbina.narudzbinaID=?" id])))
+
+
+(defn updateNarudzbina [id sirupid kolicina]
+  (sql/update! connection :narudzbina {:narudzbinaid id :sirupid sirupid :kolicina kolicina} ["narudzbinaid = ?" id]))
+
+(defn dataSirupi []
+   (into [] (sql/query connection ["SELECT * FROM sirup"])))
+
+(defn dataSearch [rez]
+  (into [] (sql/query connection ["SELECT *, SUM(narudzbina.kolicina) as broj FROM narudzbina, sirup WHERE narudzbina.sirupID = sirup.sirupID && sirup.nazivVrste LIKE ? group by sirup.sirupID" (str "%" rez "%")]))
+  )
+
+(defn addNar [sirupid kolicina]
+  (sql/insert! connection :narudzbina [:sirupid :kolicina :datumpreuzimanja] [sirupid (read-string kolicina) (java.time.LocalDateTime/now)]))
+
+
